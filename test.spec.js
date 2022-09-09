dc73da650ec0f03b00552f02f3f7793de3d7abd8
sandropark <ticoba@naver.com>
2022-09-09 11:27:18 +0900

    Add test case

---
+++ test.spec.js
@@ -0,0 +1,8 @@
+describe('Defalut Test Set', () => {
+    it('test1 should be passed', () => {
+        console.log('test1 passed')
+    })
+    it('test2 should be passed', () => {
+        console.log('test2 passed')
+    });
+});
